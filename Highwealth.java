2060f216f16284b4c014fc20de92cdb83298b579
Nishant Harde <nishanthard77@gmail.com>
2024-10-05 16:18:42 +0530

    Update Highwealth.java
    Added a commented leetcode version of code at the top of editor oriented code

--- Highwealth.java
+++ Highwealth.java
@@ -1,3 +1,22 @@
+// Simpler Leetcode solution
+
+
+// class Solution {
+//     public int maximumWealth(int[][] accounts) {
+//         int res = 0;
+//         for(int i =0;i<accounts.length;i++){
+//             int temp = 0;
+//             for(int j = 0;j<accounts[i].length;j++){
+//                 temp+=accounts[i][j];
+//             }
+//             res = Math.max(res,temp);
+//         }
+//         return res;
+//     }
+// }
+
+// solution to work in Code editor
+
 public class Highwealth {
 
     public static int maximumWealth(int[][] accounts) {
